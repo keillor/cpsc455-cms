766ed044958f452c5eef3515c639566c4c3ec98e
Kenytt Avery <kavery@fullerton.edu>
2024-11-01 04:39:40 -0700

    bin/hash.mjs should use .env as well

--- bin/hash.mjs
+++ bin/hash.mjs
@@ -1,12 +1,8 @@
-#!/usr/bin/env node
-
 import argon2 from 'argon2'
-
-const SECRET = 'cpsc455-cms'
 
 for (const passwd of process.argv.slice(2)) {
   const hash = await argon2.hash(passwd, {
-    secret: Buffer.from(SECRET)
+    secret: Buffer.from(process.env.SECRET)
   })
 
   console.log(passwd, hash)
